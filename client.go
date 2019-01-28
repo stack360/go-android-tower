fd21320a76fb41840a2438576e64d8464036a2fe
xichengc <xichengcdev@gmail.com>
2019-01-28 15:43:00 -0800

    Create basic functionalities for android tower go client.

---
+++ client.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+    "bytes"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "encoding/json"
+)
+
+type Client struct {
+    BaseURL    *url.URL
+    httpClient *http.Client
+}
+
+func NewClient(path string) *Client {
+    httpClient := http.DefaultClient
+    u, _ := url.Parse(path)
+    c := &Client {BaseURL: u, httpClient: httpClient}
+    return c
+}
+
+func (c *Client) RunFunction(funcName string, params []byte) (string, error) {
+    body := make(map[string]interface{})
+    json.Unmarshal(params, &body)
+    req, err := c.newRequest("POST", "/api/run_function", body)
+    if err != nil {
+        return "Error encountered.", err
+    }
+    resp, err := c.do(req)
+    data, _ := ioutil.ReadAll(resp.Body)
+    return string(data), nil
+}
+
+func (c *Client) newRequest(method string, path string, body map[string]interface{}) (*http.Request, error) {
+    rel := &url.URL{Path: path}
+    u := c.BaseURL.ResolveReference(rel)
+    var buf io.ReadWriter
+    if body != nil {
+        buf = new(bytes.Buffer)
+        err := json.NewEncoder(buf).Encode(body)
+        if err != nil {
+            return nil, err
+        }
+    }
+    req, err := http.NewRequest(method, u.String(), buf)
+    if err != nil {
+        return nil, err
+    }
+    if body != nil {
+        req.Header.Set("Content-Type", "application/json")
+    }
+    req.Header.Set("Accept", "application/json")
+    return req, nil
+}
+
+func (c *Client) do(req *http.Request) (*http.Response, error) {
+    resp, err := c.httpClient.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    return resp, nil
+}
